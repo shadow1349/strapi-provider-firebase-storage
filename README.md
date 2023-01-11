55a8721367ed6eb28ebd54c7647e8fe902e8ee4d
Sam Redmond <sredmond@techusolutions.com>
2023-01-11 14:05:02 -0600

    Adding code for the provider

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# strapi-provider-firebase-storage
+# Strapi Provider Firebase Storage
+
+<p align="center">
+    <img src="https://i.imgur.com/3K2Y6g3.png">
+</p>
+
+This is a Strapi provider that will upload your files to Firebase Storage.
+
+# Usage
+
+Here is a sample usage:
+
+```javascript
+module.exports = ({ env }) => {
+  return {
+    ...
+    upload: {
+      config: {
+        provider: "strapi-provider-firebase-storage",
+        providerOptions: {
+          serviceAccount: require("path/to/my/serviceAccount.json"),
+          // Custom bucket name
+          bucket: env(
+            "STORAGE_BUCKET_URL",
+            "my-bucket-name.appspot.com"
+          ),
+          sortInStorage: true, // true | false
+          debug: false, // true | false
+        },
+      },
+    },
+    ...
+  };
+};
+```
+
+# Config Options
+
+| Option           | Is Required | Default | Notes                                                                                                                      |
+| :--------------- | :---------- | :------ | :------------------------------------------------------------------------------------------------------------------------- |
+| `serviceAccount` | true        | none    | This is just the path to your service account file                                                                         |
+| `bucket`         | false       | none    | If you leave this blank it **should** go to your default bucket, but I'd recommend putting your default bucket name anyway |
+| `sortInStorage`  | false       | true    | This will sort files in your firebase storage bucket into folders                                                          |
+| `debug`          | false       | false   | This will just log all the steps to the console                                                                            |
+
+## Notes on the `sortInStorage` option
+
+By default Strapi will just output all the files in the default bucket with no folder structure (even if you make folders in the Strapi admin UI). Strapi also creates
+variants of files (like iamges ex. thumbnails) so if you upload an image to Strapi you'll actually have like a few variants of it actually uploaded to Firebase.
+
+If you were to look at that in Firebase it would look like total chaos. So what I did was create a couple of functions that will upload your files based on mime type
+as well as sort all variants of an image under a single folder. If you wanted to do something else with those images (for example create triggers for uploads for specific folders)
+they will be sorted nicely for you.
+
+Alos be really careful toggling the `sortInStorage` option. If you have it on, upload some files, then turn it off the
+delete function **could** break for the files that were uploaded when it was turned on.
---
+++ src/index.ts
@@ -0,0 +1,178 @@
+import * as admin from "firebase-admin";
+import * as path from "path";
+
+module.exports = {
+  init(config) {
+    admin.initializeApp({
+      credential: admin.credential.cert(config.serviceAccount),
+      // If you have a custom bucket this will set that bucket
+      storageBucket: config.bucket,
+    });
+
+    // We set the custom bucket in the storageBucket option in the firbase admin init config
+    const bucket = admin.storage().bucket();
+
+    /**
+     * This will help debug any potential issues with the library and can
+     * be turned on/off from the config
+     */
+    function print(message?: any, ...optionalParams: any[]) {
+      if (config.debug) console.log(message, ...optionalParams);
+    }
+
+    /**
+     * Strapi creates multiple variants of the same image for thumbnails etc.
+     * By default, Strapi will just output all the files in the base bucket and
+     * that can be kind of a pain in the ass to look at.
+     *
+     * This function will grab the base file name from file variants like
+     * "thumbnail_fileName" so I can use that as a folder path.
+     */
+    function getFileFolder(fileName: string) {
+      print("FILE NAME: ", fileName);
+
+      const tagRgx = /thumbnail_|large_|small_|medium_/gi;
+      const name = path.parse(fileName).name;
+      print("FILE NAME WITHOUT EXTENSION: ", name);
+
+      const folderName = name.split(tagRgx).find((x) => !!x);
+      print("FOLDER NAME: ", folderName);
+
+      return folderName;
+    }
+
+    /**
+     * To add a layer of organization I wanted to group all the images by their
+     * type. Since we get the MIME type from the uploaded file from Strapi we can use
+     * that to help group iamges in Firebase Storage.
+     */
+    function mimeTypeFolderGenerator(mime: string) {
+      const mimeRgx = /image|video|pdf|font|javascript|html/gi;
+
+      const matches = mime.match(mimeRgx);
+
+      print("MIME MATCHES: ", matches);
+
+      if (matches && matches.length > 0) {
+        let folderName = matches[0];
+        print("FOLDER NAME: ", folderName);
+
+        folderName = folderName.toLocaleLowerCase();
+        print("FOLDER NAME: ", folderName);
+
+        const normalizedFolderName =
+          folderName.charAt(0).toUpperCase() + folderName.slice(1);
+        print("NORMALIZED FOLDER NAME: ", normalizedFolderName);
+
+        return normalizedFolderName;
+      }
+      return "";
+    }
+
+    /**
+     * We need the file ref for all functions so it makes sense to get that here.
+     */
+    function getFileRef(file) {
+      const fileName = `${file.hash}${file.ext}`;
+      print("FILE NAME: ", fileName);
+
+      const basePath = mimeTypeFolderGenerator(file.mime);
+      print("FILE BASE PATH: ", basePath);
+
+      const fileFolderName = getFileFolder(file.hash);
+      print("FILE FOLDER NAME: ", fileFolderName);
+
+      const fullFilePath = `${
+        basePath ? `${basePath}/` : ""
+      }${fileFolderName}/${fileName}`;
+      print("FILE FULL PATH: ", fullFilePath);
+
+      return bucket.file(config.sortInStorage ? fullFilePath : fileName);
+    }
+
+    const upload = (file) =>
+      new Promise((resolve, reject) => {
+        const fileRef = getFileRef(file);
+
+        const fileURL = `https://storage.googleapis.com/${config.bucket}/${fileRef.name}`;
+        print("FILE URL: ", fileURL);
+
+        if (file.stream) {
+          const writeStream = fileRef.createWriteStream({
+            public: true,
+            contentType: file.mime,
+          });
+
+          file.stream
+            .pipe(writeStream)
+            .on("error", (error) => {
+              if (config.debug) console.error(error);
+              print("\n\n");
+              reject(error);
+            })
+            .on("finish", async () => {
+              /**
+               * We need to set the file.url because this will go into the database and
+               * will determine where the image is served from. If this isn't set then
+               * it will break.
+               */
+              file.url = fileURL;
+              print("\n\n");
+
+              // Resolve just markes that the upload is complete
+              resolve("");
+            });
+        } else if (file.buffer) {
+          const fileBuffer = Buffer.from(file.buffer);
+
+          fileRef.save(
+            fileBuffer,
+            {
+              public: true,
+              contentType: file.mime,
+            },
+            async (error) => {
+              if (error) {
+                if (config.debug) console.error(error);
+                print("\n\n");
+                reject(error);
+              }
+
+              /**
+               * We need to set the file.url because this will go into the database and
+               * will determine where the image is served from. If this isn't set then
+               * it will break.
+               */
+              file.url = fileURL;
+              print("\n\n");
+              // Resolve just markes that the upload is complete
+              resolve("");
+            }
+          );
+        }
+      });
+
+    return {
+      // We can have 1 function that handles file streams and buffers
+      upload: (file) => upload(file),
+      uploadStream: (file) => upload(file),
+      delete: (file) =>
+        new Promise((resolve, reject) => {
+          const fileRef = getFileRef(file);
+
+          fileRef
+            .delete()
+            .then(() => {
+              print("\n\n");
+              // Resolve just markes that the delete is complete
+              resolve("");
+            })
+            .catch((error) => {
+              if (config.debug) console.error(error);
+              print("\n\n");
+              reject(error);
+            });
+        }),
+    };
+  },
+};
